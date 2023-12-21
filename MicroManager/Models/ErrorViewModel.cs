d93b6b99e1e43b5a27cbc50a9b37371cf5716f35
TomMarazzo <camenm@hotmail.com>
2023-12-21 08:19:04 -0500

    Add project files.

---
+++ MicroManager/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MicroManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
