8ba6bbdc54c2f7f605062976615991d039fbb587
TomMarazzo <camenm@hotmail.com>
2023-12-27 11:55:43 -0500

    Latest Employee Updates

--- MicroManager/Migrations/20231227165047_updates4.Designer.cs
+++ MicroManager/Migrations/20231227165047_updates4.Designer.cs
@@ -4,6 +4,7 @@ using MicroManager.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MicroManager.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231227165047_updates4")]
+    partial class updates4
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- MicroManager/Models/Employee.cs
+++ MicroManager/Models/Employee.cs
@@ -16,8 +16,8 @@ namespace MicroManager.Models
         [Display(Name = "First Name")]
         public string FirstName { get; set; }
         [Display(Name = "Last Name")]
-        public string LastName { get; set; } 
-       
+        public string LastName { get; set; }
+        public string Address { get; set; }
         public string City { get; set; }    
         public string Region { get; set; }
         [Display(Name = "Postal Code")]
